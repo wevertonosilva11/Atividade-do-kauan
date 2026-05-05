8f3e0a27bd9b275b70f08df7e737f74873f63044
wevertonosilva11 <weverton.o.silva11@aluno.senai.br>
2026-05-05 11:05:23 -0500

    atualizando

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Pega os elementos da páginas pelo ID
  const campoBusca = document.getElementById('campoBusca');
  const btnBuscar = document.getElementById('btnBuscar');
  const resultado = document.getElementById('resultado')
  const msgErro = document.getElementById('msgErro');
 
+ // Função principal: busca o pokemon do APi
  async function buscarPokemon () {
     const nome = campoBusca.ariaValueMax.toLowerCase().trim();}
     
@@ -10,8 +12,39 @@
         alert('Digite o nome de um Pokemon');
         return;
     }
+
     try {
-        const resposta = await fetch('https://pokeapi.co/api/v2/pokemon/${nome}')
-        if(!resposta.ok) throw new Error('Não encontrado')
-        const dados = await resposta.json();   
+        // faz a requisição para o pokeAPI
+        const resposta = await fetch('https://pokeapi.co/api/v2/pokemon/${nome}');
+        
+        // se o pokemon não existe, lança o erro
+        if(!resposta.ok) throw new Error('Não encontrado');
+        // converte a resposta para JSON
+        const dados = await resposta.json();  
+
+        //preencha os campos com os dados do html
+        document.getElementById('pokeNome').textContent = dados.name;
+        document.getElementById('pokeID').textContent = dados.id;
+        document.getElementById('pokeAltura').textContent = dados.height;
+        document.getElementById('pokePeso').textContent = dados.weight;
+        document.getElementById('pokeTipo').textContent = dados.types [0]. type.name;
+        document.getElementById('pokeImg').src = dados.sprites.front_default;
+
+        // mostra o card e esconde o erro 
+        resultado.classList.remove('escondido');
+        msgErro.classList.add('escondido');
+
+    }catch (erro){
+     //esconde o card e mostra o erro
+     resultado.classList.add('escondido');
+     msgErro.classList.remove('escondido')
     }
+
+
+    //Dispara a busca ao clicar no botão
+    btnBuscar.addEventListener('click', buscarPokemon);
+
+    //Dispara a busca ao pressionar Enter no campo
+    campoBusca.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') buscarPokemon();
+    });
